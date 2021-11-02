3a0278c3d601145ac6a59f66e8691a6b8dddbbab
DevZero9000 <devin_savero@ymail.com>
2021-11-02 22:17:21 +0700

    upload

---
+++ app/src/main/java/com/example/moviedb/helper/Const.java
@@ -0,0 +1,9 @@
+package com.example.moviedb.helper;
+
+public class Const {
+
+    public static final String API_KEY = "9593cc7445fb21f7cb1a4ce29140d03a";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String IMG_URL = "https://image.tmdb.org/t/p/w500/";
+
+}
